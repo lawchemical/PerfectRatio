c777c7e47c99d3e66d30212d2b2c4ce29cc00c19
Sean Law <sean@lawchemical.com>
2025-08-17 20:33:51 -0400

    Revert to production server now that port is configured
    - Railway port is now correctly set to 8080
    - Health check is succeeding internally
    - Switch back to production server

--- start-admin.js
+++ start-admin.js
@@ -1,9 +1,4 @@
 // Entry point for ADMIN SERVER
 // Railway admin server should use: node start-admin.js
 console.log('Starting ADMIN SERVER...');
-
-// Temporarily use ultra-simple server to debug Railway
-require('./simple-server.js');
-
-// Uncomment to use production server
-// require('./server-production.js');
+require('./server-production.js');
